41e838c66e36b1ea954d1159b610be342c51829b
p.mardaniyan <p.mardaniyan@gmail.com>
2022-11-15 18:40:36 +0330

    create first services and repository for restaurant

--- src/restaurants/restaurants.resolver.ts
+++ src/restaurants/restaurants.resolver.ts
@@ -1,27 +1,17 @@
-
 import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
 import { createRestaurantDto } from './dto/create-restaurant.dto';
 import { Restaurant } from './entities/restaurant.entity';
+import { RestaurantService } from './restaurants.service';
 
-@Resolver(of => Restaurant)
+@Resolver((of) => Restaurant)
 export class RestaurantResolver {
+  constructor(private readonly restaurantService: RestaurantService) {}
   @Query((restaurants) => [Restaurant])
-  restaurants(): Restaurant[] {
-    return [];
+  restaurants(): Promise<Restaurant[]> {
+    return this.restaurantService.getAll();
   }
-  @Mutation(returns => Boolean)
-  createRestaurant(
-    @Args() createRestaurantDto:createRestaurantDto
-  ) : Boolean
-    
-  {
-        
-    return true
+  @Mutation((returns) => Boolean)
+  createRestaurant(@Args() createRestaurantDto: createRestaurantDto): Boolean {
+    return true;
   }
 }
-
-
-
-
-
-
